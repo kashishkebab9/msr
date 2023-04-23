0a1db5dcab12eff60ec3570e695423d4f8b3422e
Kashish Garg <kashishgarg9@gmail.com>
2023-04-22 21:34:44 -0400

    Issues with tf

--- msr_ws/src/grid_map_known_poses/include/gmwkp.h
+++ msr_ws/src/grid_map_known_poses/include/gmwkp.h
@@ -1,19 +1,36 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
+#include <Eigen/Geometry>
+#include <vector>
 
 #include "ros/ros.h"
 
 #include "geometry_msgs/PoseStamped.h"
-#include "sensor_msgs/LaserScan.h"
-#include "GridMap.h"
-#include "grid_map_ros.hpp"
+#include "sensor_msgs/PointCloud.h"
 
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+
+
+#include <grid_map_msgs/GridMap.h>
+#include <grid_map_ros/grid_map_ros.hpp>
 
 class gm_known_pose {
   public:
-    gm_known_pose() {
-      pose_sub = nh.subscribe("/scarab41/pose", 1, &gm_known_pose::poseCallback, this);
-      scan_sub = nh.subscribe("/scarab41/scan", 1, &gm_known_pose::scanCallback, this);
+    gm_known_pose() : 
+      map_frame("scarab41/map"),
+      odom_frame("scarab41/odom")
+  {
+      pose_sub = nh.subscribe("/scarab41/pose", 1, &gm_known_pose::pose_callback, this);
+      scan_sub = nh.subscribe("/pointcloud", 1, &gm_known_pose::pcl_callback, this);
+      tf_pcl_viz = nh.advertise<visualization_msgs::Marker>("tf_pcl", 1);
+
+      this->gm.add("occ_grid_map", 0.5);
+
+      //listener allows buffer to consume and store all transforms living in /tf
+      tf2_ros::TransformListener listener(this->tfBuffer);
+
     }
 
   private:
@@ -21,11 +38,21 @@ class gm_known_pose {
 
     ros::Subscriber pose_sub;
     ros::Subscriber scan_sub;
+    ros::Publisher tf_pcl_viz;
 
-    void poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
-    void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
 
-    Eigen::Vector3d current_pos;
+    void pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+    void pcl_callback(const sensor_msgs::PointCloud::ConstPtr& msg);
+    void inv_sensor_model(grid_map::Index src, grid_map::Index target);
+    std::vector<grid_map::Index> bresenham(grid_map::Index src, grid_map::Index target);
+
+    grid_map::Index robot_index;
+    grid_map::GridMap gm;
+
+    tf2_ros::Buffer tfBuffer;
+    std::string map_frame;
+    std::string odom_frame;
+
 
 
 
--- msr_ws/src/grid_map_known_poses/src/gmwkp.cpp
+++ msr_ws/src/grid_map_known_poses/src/gmwkp.cpp
@@ -1,15 +1,89 @@
 #include "gmwkp.h"
 #include <exception>
 
-void gm_known_pose::poseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
-  this->current_pos <<  msg->pose.position.x, msg->pose.position.y, 1;
+void gm_known_pose::pose_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+
+  grid_map::Position robot_position;
+  robot_position.x() = msg->pose.position.x;
+  robot_position.y() = msg->pose.position.y;
+
+  this->gm.getIndex(robot_position, this->robot_index);
 }
 
-void gm_known_pose::scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+void gm_known_pose::pcl_callback(const sensor_msgs::PointCloud::ConstPtr& msg) {
+  // Need to convert the pointcloud data to grid_map data
+  // Might be easier to use our scan_to_pcl node 
+  // And then convert to grid_map since we know the robot pose and scan data
+  // These are points relative to the laser frame, so we need to transform them into the world frame first
+  tf2_ros::Buffer tfBuffer_new;
+  tf2_ros::TransformListener listener(tfBuffer_new);
+  
+  grid_map::Index robot_index_ = this->robot_index;
+  std::vector<grid_map::Index> pcl_indices;
+
+  ros::Time time(0);
+
+  std::string * map_odom_check_err = new std::string();
+  bool map_odom_check = tfBuffer_new.canTransform(this->map_frame, this->odom_frame, time, map_odom_check_err);
+
+  if (map_odom_check) {
+
+    tf2::Stamped<tf2::Transform> t_odom_map;
+    tf2::fromMsg(tfBuffer_new.lookupTransform(this->odom_frame, this->map_frame, time), t_odom_map);
+
+    std::vector<tf2::Vector3> tf_points;
+    for (const auto& point: msg->points) {
+      tf2::Vector3 pt(point.x, point.y, 1);
+      tf2::Vector3 tf_point = t_odom_map * pt;
+      tf_points.push_back(tf_point);
+    }
+    
+    visualization_msgs::Marker transformed_points;
+    transformed_points.action = visualization_msgs::Marker::DELETEALL;
+    transformed_points.header.frame_id = "scarab41/map";
+    transformed_points.ns = "transformed_pcl";
+    transformed_points.action = visualization_msgs::Marker::ADD;
+    transformed_points.pose.orientation.w = 1.0;
+    transformed_points.id = 0;
+    transformed_points.type = visualization_msgs::Marker::POINTS;
+
+    transformed_points.scale.x = 0.05;
+    transformed_points.scale.y = 0.05;
+    transformed_points.color.g = 1.0f;
+    transformed_points.color.b = 1.0f;
+    transformed_points.color.a = 1.0;
+
+    for (auto x: tf_points)  {
+      geometry_msgs::Point point;
+      point.x = x.x();
+      point.y = x.y();
+      point.z = 0;
+      transformed_points.points.push_back(point);
+    }
+
+    this->tf_pcl_viz.publish(transformed_points);
+  }else {
+    ROS_WARN("Can't find tf!");
+    std::cout << *map_odom_check_err << std::endl;
+  }
+
+
+
+
+
 
 }
 
+void gm_known_pose::inv_sensor_model(grid_map::Index src, grid_map::Index target){
 
+}
+
+std::vector<grid_map::Index> gm_known_pose::bresenham(grid_map::Index src, grid_map::Index target){
+
+
+}
+
+//-------------------------------------------------------//
 int main(int argc, char **argv) {
   ros::init(argc, argv, "grid_mapping_with_known_poses");
   gm_known_pose solver;
